dc2c9efb11b15eae1b6ee4f917d583c0c3b008ee
katamartin <katasm@gmail.com>
2015-09-06 19:13:32 -0700

    add basic board methods

---
+++ js/board.js
@@ -0,0 +1,22 @@
+(function() {
+  if (typeof TwentyFortyEight === "undefined") {
+    window.TwentyFortyEight = {};
+  }
+
+  var Board = TwentyFortyEight.Board = function(params) {
+    this.dim = params.dim || 4;
+    this.makeGrid();
+  };
+
+  Board.prototype.makeGrid = function() {
+    var grid = new Array(this.dim);
+    for (var i = 0; i < this.dim; i++) {
+      grid[i] = new Array(this.dim);
+    }
+    this.grid = grid;
+  };
+
+  Board.prototype.isEmpty = function(pos) {
+    return !!this.grid[pos[0]][pos[1]];
+  };
+})();
