867620f5d755e31857d87bcd1813df278f5ff01f
katamartin <katasm@gmail.com>
2015-09-08 09:04:04 -0700

    remove commented code

--- js/view.js
+++ js/view.js
@@ -59,25 +59,7 @@
       });
     });
   };
-
-  // View.prototype.makeGrid = function() {
-  //   var html = "";
-  //   for (var i = 0; i < this.board.dim; i++) {
-  //     html += "<ul>";
-  //     for (var j = 0; j < this.board.dim; j++) {
-  //       var tile = this.board.get([i, j]);
-  //       if (tile) {
-  //         html += "<li class='tile-" + tile.val +  "'>" + tile.val +  "</li>";
-  //       } else {
-  //         html += "<li></li>";
-  //       }
-  //     }
-  //     html += "</ul>";
-  //   }
-  //   this.$el.html(html);
-  //   this.$li = this.$el.find("li");
-  // };
-
+  
   View.prototype.makeGrid = function() {
     var html = $("<div></div>");
     for (var i = 0; i < this.board.dim; i++) {
